d5d7a1e237b573d9f6ad5b25cef6eeabded34260
droid9211 <96025384+droid9211@users.noreply.github.com>
2022-05-18 00:34:05 +0300

    add header-, nav-menu & footer

--- pages/posts/_id.vue
+++ pages/posts/_id.vue
@@ -1,13 +1,31 @@
 <template>
-  <section id="/post/_id" class="max-w-md m-36 p-8 mx-auto bg-indigo-50 rounded-xl shadow-md overflow-hidden md:max-w-xl">
-    <div class="md:block">
-      <p v-if="$fetchState.pending">Loading....</p>
-      <p v-else-if="$fetchState.error">Error while fetching posts</p>
-      <h1 class ="font-extrabold mb-6 text-gray-700 text-2xl tracking-wide">{{ post.title }}</h1>
-      <p class="text-left text-base">{{ post.body }}</p>
-    </div>
-    <nuxt-link to="/posts" tag="li" class="back mt-6 font-medium"><a>Back to Posts</a></nuxt-link>
-  </section>
+  <NuxtLayout>
+    <header>
+      <nav class="nav bg-gray-100 shadow-xl">
+        <ul class="flex space-x-2 p-4 rounded-md text-xl bg-amber-100 text-amber-700 font-semibold">
+          <nuxt-link to="/" tag="li"><a>Home</a></nuxt-link>
+          <nuxt-link to="/posts" tag="li"><a>Posts</a></nuxt-link>
+        </ul>
+      </nav>
+    </header>
+
+    <section id="/post/_id"
+      class="max-w-md m-36 p-8 mx-auto bg-indigo-50 rounded-xl shadow-md overflow-hidden md:max-w-xl">
+      <div class="md:block">
+        <p v-if="$fetchState.pending">Loading....</p>
+        <p v-else-if="$fetchState.error">Error while fetching posts</p>
+        <h1 class="font-extrabold mb-6 text-gray-700 text-2xl tracking-wide">{{ post.title }}</h1>
+        <p class="text-left text-base">{{ post.body }}</p>
+      </div>
+      <nuxt-link to="/posts" tag="li" class="back mt-6 font-medium"><a>Back to Posts</a></nuxt-link>
+    </section>
+
+    <footer class="main-footer font-sans bg-gray-100 shadow-inner">
+      <div>
+        <p> © 2022 – Made by Lilly A. Nikolova</p>
+      </div>
+    </footer>
+  </NuxtLayout>
 </template>
 
 <script>
